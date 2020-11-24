5614ec2364b4694b4ce65e1366cd24a0f4340e37
Roman Lobanets <romanlobanets@Romans-MacBook-Pro.local>
2020-11-23 20:53:06 -0500

    done

--- dist/helpers/formatErrors.js
+++ dist/helpers/formatErrors.js
@@ -1,22 +1,24 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+  value: true,
 });
 
-var _lodash = require("lodash");
+var _lodash = require('lodash');
 
-var _lodash2 =      _interopRequireDefault(_lodash);
+var _lodash2 = _interopRequireDefault(_lodash);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 var formatErrors = function formatErrors(error, models) {
   if (error instanceof models.Sequelize.ValidationError) {
     return error.errors.map(function (x) {
-      return _lodash2.default.pick(x, ["path", "message"]);
+      return _lodash2.default.pick(x, ['path', 'message']);
     });
   }
-  return [{ path: "name", message: "something went wrong" }];
+  return [{ path: 'name', message: 'something went wrong' }];
 };
 
 exports.default = formatErrors;
--- src/models/index.js
+++ src/models/index.js
@@ -1,30 +1,31 @@
-import Sequelize from "sequelize";
+import Sequelize from 'sequelize';
 
 // const sequelize = new Sequelize("slack", "avnadmin", "jhek8h30m7lf24y7", {
 //   dialect: "postgres",
 // });
 const sequelize = new Sequelize(
-  "postgres://avnadmin:jhek8h30m7lf24y7@pg-1fee7ce6-lobanets-dd72.aivencloud.com:25014/slack?sslmode=require",
+  'postgres://avnadmin:saizszmufvlzuvw0@pg-266e3494-extreme-cb0c.aivencloud.com:26522/defaultdb?sslmode=require',
   {
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false,
       },
+      host: process.env.DB_HOST || 'localhost',
     },
     define: { undescored: true },
   }
 );
 
 const models = {
-  User: sequelize.import("./users"),
-  Channel: sequelize.import("./channel"),
-  Message: sequelize.import("./message"),
-  Team: sequelize.import("./team"),
-  Member: sequelize.import("./member"),
-  PCMember: sequelize.import("./pcmember"),
+  User: sequelize.import('./users'),
+  Channel: sequelize.import('./channel'),
+  Message: sequelize.import('./message'),
+  Team: sequelize.import('./team'),
+  Member: sequelize.import('./member'),
+  PCMember: sequelize.import('./pcmember'),
 };
 Object.keys(models).forEach((modelName) => {
-  if ("associate" in models[modelName]) {
+  if ('associate' in models[modelName]) {
     models[modelName].associate(models);
   }
 });
