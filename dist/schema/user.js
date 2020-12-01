7f21c05dd8a98bd49661bf99a21b721427420a99
Roman Lobanets <romanlobanets@Romans-MacBook-Pro.local>
2020-11-30 19:11:07 -0500

    remove dist

--- dist/schema/user.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = "\n  type User {\n    id: Int!\n    username: String!\n    email: String!\n    messages: Message!\n    teams: [Team!]!\n  }\n  \n  type Query{\n    me:User!\n    allUsers:[User!]!\n    getUser(userId: Int!): User\n  }\n\n  type RegisterResponce{\n    ok: Boolean!\n    user: User\n    errors:[Error!]\n  }\n  type LoginResponce{\n    ok: Boolean!\n    token: String\n    refreshToken: String \n    errors:[Error!]\n  }\n\n  type Mutation{\n    register(username:String!,email:String!,password:String!):RegisterResponce!\n    login(email:String!,password:String!):LoginResponce!\n  }\n";
